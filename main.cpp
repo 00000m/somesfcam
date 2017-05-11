4736e9a678267d92f0149722a8a8915ec82f2147
000m <perk0srak@yandex.com>
2017-05-11 20:57:50 +0400

    add sfml code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+#include <SFML\Graphics.hpp>
 #include <iostream>
 using namespace std;
 
+
+
 int main()
 {
-	cout<<"Hi, git!"<<endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Hello, sfml!");
+	sf::Event event;
+	while (!event.Closed)
+	{
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			default:
+				break;
+			}
+		}
+
+
+		window.clear(sf::Color::White);
+		window.display();
+	}
 	return 0;
 }
